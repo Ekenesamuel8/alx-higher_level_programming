5a4d50e24f3d45b8bb787e6ff84f700c60e63643
ekene samuel <chikwenduekene@gmail.com>
2023-11-08 00:02:01 +0000

    dds

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        print("")
+    if column not len(matrix[row]) -1
+        endspace = " "
+    else:
+        print("{:d}".format(matrix[row][column), end = " ")
+        print("")
---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    if not sentence:
+        return ""
+    else:
+        return (sentence[0], sentence[0])
+        
