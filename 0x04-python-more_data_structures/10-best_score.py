06e54a7197a2b9a800915fd05d7107e8fe8f54d8
ekene samuel <chikwenduekene@gmail.com>
2023-11-08 23:07:13 +0000

    0x04python-more_data_structures

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+
+def best_score(my_dict):
+    return max(my_dict, key=my_dict.get) if my_dict else None
---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+def simple_delete(my_dict, key=""):
+    my_dict.pop(key, None)
+    return my_dict
