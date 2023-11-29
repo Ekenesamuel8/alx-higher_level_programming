59f0566b7bd650a18c4e17f357cb957fd3108000
ekene samuel <chikwenduekene@gmail.com>
2023-11-29 00:38:23 +0000

    tsk 0 and 1

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""defining class"""
+
+class Rectangle:
+    """empty class
+    """
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,2 @@
+more problems to be solve on python classes and objects
+alx daily tasks
