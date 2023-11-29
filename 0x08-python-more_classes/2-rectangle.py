98576c71f21c6f9eec7577eb55cc889572e94ad0
ekene samuel <chikwenduekene@gmail.com>
2023-11-29 02:25:21 +0000

    ddd

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -2,6 +2,10 @@
 """defines class Rectangle"""
 
 class Rectangle:
+    """Instantiation with optional
+        arg:
+        width and height
+    """
     def __init__(self, width=0, height=0):
         self.__width = width
         self.__height = height
@@ -16,10 +20,19 @@ class Rectangle:
 
     @property
     def width(self):
+        """property def width(self): to retrieve it
+        """
         return self.__width
 
     @width.setter
     def width(self, value):
+        """property setter def width(self, value)
+            width must be an integer
+            otherwise raise a TypeError exception
+            if width is less than 0,
+	    raise a ValueError exception
+        """
+
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value < 0:
@@ -29,10 +42,19 @@ class Rectangle:
 
     @property
     def height(self):
+        """property def width(self): to retrieve it
+        """
         return self.__height
 
     @height.setter
     def height(self, value):
+        """property setter def width(self, value)
+            width must be an integer
+            otherwise raise a TypeError exception
+            if width is less than 0,
+	    raise a ValueError exception
+        """
+
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value < 0:
--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -3,15 +3,29 @@
 
 class Rectangle:
     def __init__(self, width=0, height=0):
+        """Instantiation with optional
+        arg:
+        width and height
+        """
+
         self.__width = width
         self.__height = height
 
     @property
     def width(self):
+        """property def width(self): to retrieve it
+        """
         return self.__width
 
     @width.setter
     def width(self, value):
+        """property setter def width(self, value)
+            width must be an integer
+            otherwise raise a TypeError exception
+            if width is less than 0,
+	    raise a ValueError exception
+        """
+
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value < 0:
@@ -21,10 +35,19 @@ class Rectangle:
 
     @property
     def height(self):
+        """property def width(self): to retrieve it
+        """
         return self.__height
 
     @height.setter
     def height(self, value):
+        """property setter def width(self, value)
+            width must be an integer
+            otherwise raise a TypeError exception
+            if width is less than 0,
+	    raise a ValueError exception
+        """
+
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value < 0:
