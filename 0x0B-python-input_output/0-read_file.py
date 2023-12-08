1ab3b10257f4acb994d990d0b8fe70cedc143255
ekene samuel <chikwenduekene@gmail.com>
2023-12-08 14:06:36 +0000

    some correction

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,11 +1,13 @@
 #!/urs/bin/python3
+"""python iput and output task 0"""
 
 
 def read_file(filename=""):
-    """THIS reads a file text and rints it to stdout
+    """THIS reads a file text and prints it to stdout
     arg:
-        filename(the source file)
+        filename: file to bew open
     """
+    
     with open(filename, 'r', encoding="UTF-8") as file:
         plsread = file.read()
         print(plsread)
--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,6 +1,14 @@
 #!/urs/bin/python3
+"""python input/output task 2"""
+
 
 def append_write(filename="", text=""):
+    """append a string at the end of a textfile and return the number of character added
+    args
+        filename: text file to be open
+        text: string to be append
+    """
+
     with open(filename, 'a') as file:
-        eappend = file.write(text)
-        return (eappend)
+        file.write(text)
+        return len(text)
