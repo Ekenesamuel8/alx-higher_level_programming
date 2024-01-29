3b3f784bea22bbe30f26226b15a54a2b22843fa9
ekene samuel <chikwenduekene@gmail.com>
2024-01-29 14:37:19 +0000

    update on class and object

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""class"""
+
+
+class Rectangle:
+    """define a rectangle
+       intance:
+       width and height
+    """
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def height(self):
+        """property def height(self): to retrive it
+        """
+
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """property setter height must be an
+           integer otherwise raise raise a typerror
+           excxeption
+           if height is less than 0
+           raise vaueerror exception
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    @property
+    def width(self):
+        """property to retrive it
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """property setter width must be an
+           integer otherwise raise raise a typerror
+           excxeption
+           if width is less than 0
+           raise vaueerror exception
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,4 @@
+more and advance explaination of python classes and objects
+each tasks contains explanations or exercice or example of the article that was explained
+
+#ALX SWE
