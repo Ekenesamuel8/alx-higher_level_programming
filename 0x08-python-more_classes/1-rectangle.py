beab95bada3e473fb02b8e8518a6db9a27008d91
ekene samuel <chikwenduekene@gmail.com>
2024-01-29 14:55:14 +0000

    class and object

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -12,6 +12,27 @@ class Rectangle:
         self.__height = height
 
     @property
+    def width(self):
+        """property to retrive it
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """property setter width must be an
+           integer otherwise raise raise a typerror
+           excxeption
+           if width is less than 0
+           raise vaueerror exception
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
     def height(self):
         """property def height(self): to retrive it
         """
@@ -32,24 +53,3 @@ class Rectangle:
             raise ValueError("height must be >= 0")
         else:
             self.__height = value
-
-    @property
-    def width(self):
-        """property to retrive it
-        """
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        """property setter width must be an
-           integer otherwise raise raise a typerror
-           excxeption
-           if width is less than 0
-           raise vaueerror exception
-        """
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        elif value < 0:
-            raise ValueError("width must be >= 0")
-        else:
-            self.__width = value
