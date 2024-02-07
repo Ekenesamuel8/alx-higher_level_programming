f89580427d31939b505a0b40fdfd51e107b5f1d6
ekene samuel <chikwenduekene@gmail.com>
2024-02-07 20:51:38 +0000

    python input and output

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""task 6 of python input output"""
+import json
+
+
+def load_from_json_file(filename):
+    """creates an Object from a "JSON file"
+       arg:
+       filename: the file
+    """
+
+    with open(filename) as file:
+        return json.load(file)
---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""python input aNd output task 9"""
+
+
+class Student:
+    """class that defines student data
+       Args:
+          first_name (str): given name of student
+          last_name (str): family name of student
+          age (int): age of student in years
+
+       Attributes:
+       first_name (str): given name of student
+       last_name (str): family name of student
+       age (int): age of student in years
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """retrieves a dictionary representation of a student instance
+        """
+
+    return self.__dict__
