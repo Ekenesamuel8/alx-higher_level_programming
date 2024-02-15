800dd043a33353b073d3f4f767c73a653b6bf64c
ekene samuel <chikwenduekene@gmail.com>
2024-02-15 14:54:06 +0000

    added init

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(id=id, x=x, y=y, width=size, heigth=size)
+        super().__init__(id=id, x=x, y=y, width=size, height=size)
         
     @property
     def size(self):
