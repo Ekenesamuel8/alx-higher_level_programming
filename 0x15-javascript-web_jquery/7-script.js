b839437bc4d1779484d275644e2fa9025fb18e44
ekenesamuel8 <chikwenduekene8@gmail.com>
2024-06-04 17:35:34 -0700

    jquery

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,10 @@
+
+Executable File·7 lines (6 loc) · 256 Bytes
+// Fetches and replaces the name of the API data then replaces the name
+// of the character in the DIV#character tag text
+
+let url = 'https://swapi.co/api/people/5/?format=json';
+$.get(url, function (data, stat) {
+  $('div#character').text(data.name);
+});
+
